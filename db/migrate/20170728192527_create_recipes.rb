7ca442977e330973a979a14a4b390aafaff69aae
ZacharyAkers <user@users-air.lan>
2017-07-28 15:28:04 -0400

    added recipe database

---
+++ db/migrate/20170728192527_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.decimal :cook_time
+      t.decimal :prep_time
+
+      t.timestamps
+    end
+  end
+end
